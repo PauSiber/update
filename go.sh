8980d96721ce8dff31053ff418609aad548e2f28
boratanrikulu <boratanrikulu@gmail.com>
2020-03-28 18:50:17 +0300

    Fix duplicated codes on go.sh

--- go.sh
+++ go.sh
@@ -8,56 +8,57 @@ die() {
   exit 444
 }
 
+clear_screen() {
+  clear
+  printf "\e[3J"
+}
+
+do_you_confirm() {
+  while :
+  do
+    echo ""
+    echo ""
+    echo -en "$1 Do you confirm? [y/n] "
+    read answer
+    echo ""
+    case "$answer" in
+      [yY] ) echo -e "Installation was started.\nPlease do not cancel the operation.\nOtherwise your system can be broke." && break ;;
+      [nN] ) die "Okay. Update service is cancelled." && break ;;
+      *    ) clear_screen && echo "(!) Please use only Y or N" ;;
+    esac
+  done
+}
+
 install_go() {
+  # Installs GVM if it is not.
   if [[ ! -s "/home/dev/.gvm/" ]]; then
-    while :
-    do
-      echo ""
-      echo ""
-      echo "Installing Update System..."
-      sleep 3
-      echo ""
-      echo -e "Update system needs go environment."
-      echo -en "Gvm will be installed. Do you confirm? [y/n] "
-      read answer
-      echo ""
-      case "$answer" in
-        [yY] ) echo -e "Installation was started.\nPlease do not cancel the operation.\nOtherwise your system can be broke." && break ;;
-        [nN] ) die "Okay. Update service is cancelled." && break ;;
-        *    ) clear && echo "(!) Please use only Y or N" ;;
-      esac
-    done
+    echo ""
+    echo ""
+    echo "Installing Update System..."
+    sleep 3
+    echo ""
+    echo -e "Update system needs go environment."
+    do_you_confirm "Gvm will be installed."
     zsh < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer) >/dev/null 2>&1
     source /home/dev/.gvm/scripts/gvm
     echo -e "Installation was completed."
     echo -e `gvm version`
     echo -e "[enter]"
     read answer
-    clear
+    clear_screen
   fi
 
   source /home/dev/.gvm/scripts/gvm
+  # Installs go if it is not.
   if [[ ! -s "/home/dev/.gvm/gos/go${GO_VERSION}/bin/go" ]]; then
-    while :
-    do
-      echo ""
-      echo ""
-      echo -en "Go ${GO_VERSION} will be installed. Do you confirm? [y/n] "
-      read answer
-      echo ""
-      case "$answer" in
-        [yY] ) echo -e "Installation was started.\nPlease do not cancel the operation.\nOtherwise your system can be broke." && break ;;
-        [nN] ) die "Okay. Update service is cancelled." && break ;;
-        *    ) clear && echo "(!) Please use only Y or N" ;;
-      esac
-    done
+    do_you_confirm "Go ${GO_VERSION} will be installed."
     gvm install go${GO_VERSION} -B >/dev/null 2>&1
     gvm use go${GO_VERSION} --default >/dev/null 2>&1
     echo -e "Installation was completed."
     echo -e `go version`
     echo -e "[enter]"
     read answer
-    clear
+    clear_screen
   fi
 }
 
