d0176efd9af9d977acd252ce6aebca7d95af0399
Emrullah <emrullahcirit20@gmail.com>
2020-04-11 11:44:43 +0300

    Add NodeJS env

---
+++ updates/2_add_nodejs_env.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+WORKING_DIRECTORY="/home/dev"
+
+die() {
+  echo "$*" >&2
+  exit 444
+}
+
+success() {
+  echo "$*" >&2
+  exit 0
+}
+
+main() {
+  pacman -S nodejs npm
+  echo "NodeJS and npm installed to system."
+  success "You can use NodeJS on your terminal."
+}
+
+cd ${WORKING_DIRECTORY}
+main
